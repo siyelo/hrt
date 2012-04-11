320fa634b1875ade4759219394664f6c6ab1f9d8
Dalibor Nasevic and Sachin Ranchod <dev+dalibor+sachin@siyelo.com>
2012-04-11 17:11:54 +0200

    Fixed spork setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,6 +48,7 @@ Spork.prefork do
 end
 
 Spork.each_run do
+  require File.expand_path(File.join(File.dirname(__FILE__), 'support', 'deferred_garbage_collection'))
   # This code will be run each time you run your specs.
 
   # --- Instructions ---
