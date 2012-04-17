57451977242583cfef53ae4c913df9b06bb7865e
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-04-17 10:12:45 +0200

    Small design fixes

--- app/helpers/admin/documents_helper.rb
+++ app/helpers/admin/documents_helper.rb
@@ -1,6 +1,6 @@
 module Admin::DocumentsHelper
 
   def current_document_name(document)
-    "Current file: #{@document.document_file_name}" unless document.new_record?
+    "Current file: #{document.document_file_name}" unless document.new_record?
   end
 end
