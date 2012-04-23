30e6c810d5bcf34db3b6fe9cd2367a43a5ce5b9f
Glenn Roberts <glenn.roberts@siyelo.com>
2012-04-23 18:07:47 +0200

    fix failing migration

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(:version => 20120423084709) do
     t.datetime "updated_at"
   end
 
+  add_index "code_assignments", ["activity_id", "code_id", "type"], :name => "index_code_assignments_on_activity_id_and_code_id_and_type"
   add_index "code_assignments", ["code_id"], :name => "index_code_assignments_on_code_id"
 
   create_table "codes", :force => true do |t|
