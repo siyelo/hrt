f75c5e91f9d261c9e26150a8a119b6fa43e6b987
Sachin Ranchod <sachin.ranchod@gmail.com>
2012-05-07 11:11:43 +0200

    [fixes #29154847] Remove Admin set_latest_response

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,10 +1,3 @@
 class Admin::BaseController < ApplicationController
   before_filter :require_admin
-
-  before_filter :set_latest_response
-
-  private
-    def set_latest_response
-      set_response(last_response)
-    end
 end
