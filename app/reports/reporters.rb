0779c8445731349e9336c2ead74798915f3fc526
Peter Garbers <peter.garbers@gmail.com>
2012-05-14 17:48:51 +0200

    [Fixes #27771705] Top reporters report
    *Refactored query so that the page loads significantly faster

--- app/reports/reporters.rb
+++ app/reports/reporters.rb
@@ -1,9 +1,15 @@
 require 'app/reports/base'
 require 'app/charts/base'
 require 'app/reports/row'
+require 'lib/currency_number_helper'
 
 module Reports
   class Reporters < Reports::Base
+    include CurrencyNumberHelper
+
+    ### Constants
+    NUMBER_OF_VALUES_IN_CHARTS = 10
+
     def initialize(request)
       @resource = request
     end
@@ -12,32 +18,69 @@ module Reports
       "USD"
     end
 
-    ### uses reports::rows because it has to calculate total_spend and total_budget
-    # every time during the sort because it's not a stored field
     def collection
-      responses
-    end
-
-    def total_budget
-      responses.inject(0) do |sum, e|
-        sum + (e.total_budget || 0)
-      end
+      @collection ||= create_rows
     end
 
     def total_spend
-      responses.inject(0) do |sum, e|
+      collection.inject(0) do |sum, e|
         sum + (e.total_spend || 0)
       end
     end
 
+    def total_budget
+      collection.inject(0) do |sum, e|
+        sum + (e.total_budget || 0)
+      end
+    end
+
+    def expenditure_pie
+      Charts::Spend.new(top_spenders).google_pie
+    end
+
+    def budget_pie
+      Charts::Budget.new(top_budgeters).google_pie
+    end
+
     private
 
-    def responses
-      @responses ||= @resource.data_responses.find(:all,
-        :include => [ :organization,
-          {:projects => {:activities => :implementer_splits} },
-          {:other_costs => :implementer_splits} ],
-        :order => 'organizations.name')
+    def create_rows
+      rows.map do |split|
+        if split.tot_spend.to_f > 0 && split.tot_budget.to_f > 0
+          org_currency = split.currency
+          Reports::Row.new(split.org_name,
+            universal_currency_converter(split.tot_spend.to_f, org_currency, "USD"),
+            universal_currency_converter(split.tot_budget.to_f, org_currency, "USD"))
+        end
+      end.compact
+    end
+
+    def top_spenders
+      collection.sort{ |x, y| y.total_spend <=> x.total_spend }.
+        first(NUMBER_OF_VALUES_IN_CHARTS)
+    end
+
+    def top_budgeters
+      collection.sort{ |x, y| y.total_budget <=> x.total_budget }.
+        first(NUMBER_OF_VALUES_IN_CHARTS)
+    end
+
+    def rows
+      @rows = ImplementerSplit.find(:all,
+               :joins => "INNER JOIN activities ON
+                   activities.id = implementer_splits.activity_id
+                   INNER JOIN data_responses ON
+                   activities.data_response_id = data_responses.id
+                   INNER JOIN data_requests ON
+                   data_responses.data_request_id = data_requests.id
+                   INNER JOIN organizations ON
+                   data_responses.organization_id = organizations.id",
+               :select => "organizations.name AS org_name,
+                    implementer_splits.currency AS currency,
+                    SUM(implementer_splits.spend) AS tot_spend,
+                    SUM(implementer_splits.budget) AS tot_budget",
+               :conditions => "data_responses.data_request_id = #{@resource.id}",
+               :group => "organizations.name, implementer_splits.currency" )
     end
   end
 end
