42cb28c1654f8b7e37da44d96caadc38dd5b471a
Sachin Ranchod <sachin.ranchod@gmail.com>
2012-05-17 15:14:25 +0200

    remove Implementer Split currency data fix

--- db/migrate/20120514131221_add_currencies_to_splits.rb
+++ db/migrate/20120514131221_add_currencies_to_splits.rb
@@ -1,16 +1,6 @@
 class AddCurrenciesToSplits < ActiveRecord::Migration
   def self.up
     add_column :implementer_splits, :currency, :string
-    ImplementerSplit.reset_column_information
-    is = ImplementerSplit.all
-    count = is.count
-    i = 0
-    is.each do |split|
-      i += 1
-      puts "Adding currency to implementer_splits #{ i }/#{ count }"
-      split.currency = split.activity.currency
-      split.save(false)
-    end
   end
 
   def self.down
