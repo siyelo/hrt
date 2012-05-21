be92971ff7b5b123c17c766682299ee795e656e8
Glenn Roberts <glenn.roberts@siyelo.com>
2012-05-21 10:05:00 +0200

    increase pie visibility threshold

--- public/javascripts/hrt.charts.js
+++ public/javascripts/hrt.charts.js
@@ -34,6 +34,6 @@ HrtCharts.drawPieChart = function (element, data_rows, width, height) {
   var chart = new google.visualization.PieChart(element);
   chart.draw(data, {width: width, height: height,
              chartArea: {width: 360, height: 220},
-             sliceVisibilityThreshold: 1/120
+             sliceVisibilityThreshold: 1/60
   });
 };
