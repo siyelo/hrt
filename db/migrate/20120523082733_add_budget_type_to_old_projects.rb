b6dbd0accaf3ff4d0a6b470750b91bee1704c320
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-05-31 11:09:42 +0200

    Fixed project budget_type data fix
      * When running all migrations on production db it was not setting
        default budget_type to "na"

--- db/migrate/20120523082733_add_budget_type_to_old_projects.rb
+++ db/migrate/20120523082733_add_budget_type_to_old_projects.rb
@@ -1,5 +1,6 @@
 class AddBudgetTypeToOldProjects < ActiveRecord::Migration
   def self.up
+    Project.reset_column_information
     load 'db/fixes/20120523_add_budget_type_to_old_projects.rb'
   end
 
