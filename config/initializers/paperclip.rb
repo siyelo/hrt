df81fa27209b26d2f26c66aa1f4d7d69f3e571c1
Dalibor Nasevic and Sachin Ranchod <dev+dalibor+sachin@siyelo.com>
2012-06-07 13:51:14 +0200

    [fixes #27771741, #30815433] Reports are stored on S3 using request specific folders

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,6 +2,10 @@ Paperclip.interpolates :key do |attachment, style|
   attachment.instance.key
 end
 
+Paperclip.interpolates :data_request_id do |attachment, style|
+  attachment.instance.data_request_id
+end
+
 Paperclip.interpolates :document_file_name do |attachment, style|
   attachment.instance.document_file_name
 end
