8dbe9ef3471db71958c1389dc125b0a2bf1dea53
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-06-19 15:27:34 +0200

    Formtastic deprecation warning, cucumber & staging setup

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -28,7 +28,7 @@ Hrt::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -65,5 +65,5 @@ Hrt::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'hrt-staging.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'staging.hrtapp.com' }
 end
