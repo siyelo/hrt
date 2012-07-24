282188be658f5b463f62a82adb40e436d0453911
Sachin Ranchod <sachin.ranchod@gmail.com>
2012-07-24 09:41:21 +0200

    fix migration failing on empty db

--- db/migrate/20120713081025_rename_other_code.rb
+++ db/migrate/20120713081025_rename_other_code.rb
@@ -1,13 +1,17 @@
 class RenameOtherCode < ActiveRecord::Migration
   def up
-    code = Code.find(1714)
-    code.short_display = 'Other inputs' if code.short_display = 'Other'
-    code.save!
+    code = Code.find_by_id(1714)
+    if code
+      code.short_display = 'Other inputs' if code.short_display = 'Other'
+      code.save!
+    end
   end
 
   def down
-    code = Code.find(1714)
-    code.short_display = 'Other'
-    code.save!
+    code = Code.find_by_id(1714)
+    if code
+      code.short_display = 'Other'
+      code.save!
+    end
   end
 end
