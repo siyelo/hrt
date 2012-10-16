a55a4b78ee31bb53070036c8c397ff8e0d3d4a80
Sachin Ranchod and Vane Andonov <dev+sachin+vane@siyelo.com>
2012-10-16 12:16:14 +0000

    add spec to check that latest response is shown after login

--- spec/integration/reporter/can_navigate_previous_response_spec.rb
+++ spec/integration/reporter/can_navigate_previous_response_spec.rb
@@ -26,4 +26,26 @@ describe "Reporter" do
     page.should_not have_content("Request 1")
     page.should have_content("Request 2")
   end
+
+  it "can always see latest response after signing in" do
+    FactoryGirl.create(:data_request, organization: organization,
+                       name: "Request 1", start_date: "2010-01-01")
+    FactoryGirl.create(:data_request, organization: organization,
+                       name: "Request 2", start_date: "2011-01-01")
+
+    login_as(reporter)
+
+    click_link "Projects & Activities"
+    page.should have_content("Request 2")
+    click_link "Previous Request"
+    page.should have_content("Request 1")
+    click_link "Sign Out"
+
+    login_as(reporter)
+
+    click_link "Projects & Activitie"
+    page.should have_content("Request 2")
+    page.should_not have_content("Request 1")
+
+  end
 end
