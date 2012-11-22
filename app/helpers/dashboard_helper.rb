4b0ae84f8b62c4364fd1b9c89c11d02db5f77ef0
Dalibor Nasevic and Sachin Ranchod <dev+dalibor+sachin@siyelo.com>
2012-11-22 13:06:17 +0100

    Fix activity manager dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -19,9 +19,10 @@ module DashboardHelper
 
   def response_log_tooltip(data_response)
     tip = data_response.status
-    if data_response.response_state_logs.last
-      log_name = data_response.response_state_logs.last.user.name
-      log_date = data_response.response_state_logs.last.created_at.strftime("%d-%m-%Y")
+    last_state_log = data_response.response_state_logs.last
+    if last_state_log && last_state_log.user
+      log_name = last_state_log.user.name
+      log_date = last_state_log.created_at.strftime("%d-%m-%Y")
       tip += "<br/>Actioned by: #{log_name}<br/> Actioned on: #{log_date} "
     else
       tip
